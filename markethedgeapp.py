b0157921dcb9ba1d8b2b23e1c37e1a9d06853b22
peterlee123451 <68183651+peterlee123451@users.noreply.github.com>
2025-03-06 18:01:15 +1100

    Add files via upload

---
+++ markethedgeapp.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import pandas as pd
+import pandas_datareader as pdr
+import numpy as np
+import matplotlib.pyplot as plt
+
+st.title("Market Hedge Tool")
+
+stock_ticker = st.text_input("Enter a stock ticker:", None)
+
+
+
+
+
+
+
+
+
